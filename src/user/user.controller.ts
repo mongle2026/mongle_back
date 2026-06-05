33687109e52eee0ce02c215821d4da06a60903f8
Yoojin-J <syoojin03@gmail.com>
2026-06-05 20:22:07 +0900

    fix: user.controller 수정
    await 추가 했음

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,10 +3,10 @@ import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Get('search')
   async searchUsers(@Query('keyword') keyword: string) {
-    return this.userService.searchUsers(keyword);
+    return await this.userService.searchUsers(keyword);
   }
 }
